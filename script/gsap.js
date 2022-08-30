d4dedad78855505745ea09676de6a77b4eeb5175
Elizabeth1047 <egharevbalizzy5@gmail.com>
2022-08-30 06:56:26 -0700

    addition of functionality

--- script/gsap.js
+++ script/gsap.js
@@ -1,44 +1,46 @@
-const timeline = gsap.timeline({ default: { duration: 2 } });
-timeline
-  .to(".btn", {
-    opacity: 1,
-    y: "0%",
-    ease: "bounce", 
-    duration: 2.2,
-  });
-  // const tmeline = gsap.timeline({ default: { duration: 2,  } });
-  // tmeline.to(".pack", {
-  //   opacity: 1,
-  //   y: "0%",
-  //   ease: "bounce",
-  //   duration: 2.2,
-  //   delay: 5
-  // });
-  
-// gsap.to(".btn", {
-//   ease: "bounce",
-//   y: 0,
-//   duration: 2.2,
-// });
+//  const timeline = gsap.timeline({ default: { duration: 2 } });
+//  timeline.to(".close", {
+//    opacity: 1,
+//    y: "0%",
+//    ease: "bounce",
+//  });
+gsap.to(".btn", {
+      opacity: 1,
+      y: "0%",
+      ease: "bounce", 
+      duration: 2.2,
+    });
 
-let button=document.querySelector(".btn")
-console.log(button)
-button.addEventListener("click", ()=>{
-  timeline.reverse()
-  gsap.to(".pack", {
-    opacity: 1,
-    y: "0%",
-    ease: "bounce",
-    duration: 2,
-    delay: 2
-  });
-  gsap.to(".h1", {
-    y: "0% ",
-    ease: "power4.inOut",
-    "clip-path": "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
-    duration: 2,
-    opacity: 1,
-    delay: 5,
-  });
-})
+
+
+
+//target element
+const modal = document.querySelector("#modal__container");
+const openModal = document.querySelector("#open");
+const closeModal = document.querySelector("#close");
+const inp = document.querySelector("#inp");
+//functinality
+openModal.addEventListener("click", () => {
+ modal.showModal();
+
+});
+closeModal.addEventListener("click", () => {
+  modal.close();
+  console.log(inp.value);
+  let action = inp.value;
+  let div = document.createElement("div");
+  let h3 = document.createElement("h3");
+  h3.textContent = action;
+  console.log(h3);
+  inp.value = " ";
+
+  div.append(h3);
+  div.classList.add("action__container");
+  h3.classList.add("action__text");
+  //   i have no idea where to append this into the regular page (deji go run this one)
+  // 
+  //   document.
+  
+});
+
 
