bd9552b8040e50004db6195301c3233c7cd2912b
oladips <oladeleoladipupo555@gmail.com>
2022-09-05 09:38:51 +0100

    scattered thoughts

--- script/app.js
+++ script/app.js
@@ -1,6 +1,13 @@
-const shape = document.querySelectorAll(".shape");
-for (let i = 0; i < shape.length; i++) {
-  shape[i].addEventListener("click", () => {
-    console.log("i got clicked");
-  });
-}
+// is there is a better way to group them today yiu can attempt it
+// const shape = document.querySelectorAll(".shape");
+// for (let i = 0; i < shape.length; i++) {
+//   shape[i].addEventListener("click", () => {
+//     console.log("i got clicked");
+//   });
+// }
+//individual targetting
+const tri = document.querySelector(".tri");
+const hex = document.querySelector(".hex");
+const circle = document.querySelector(".round");
+const star = document.querySelector(".star-shape");
+const rec = document.querySelector(".rec");
--- script/gsap.js
+++ script/gsap.js
@@ -1,9 +1,9 @@
 gsap.to(".btn", {
-      opacity: 1,
-      y: "0%",
-      ease: "bounce", 
-      duration: 2.2,
-    })
+  opacity: 1,
+  y: "0%",
+  ease: "bounce",
+  duration: 2.2,
+});
 
 const timeline = gsap.timeline({ defaults: { duration: 1, delay: 1 } });
 timeline
@@ -12,8 +12,8 @@ timeline
     y: "0%",
     ease: "power4.inOut",
     "clip-path": "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
-   duration: 1,
-    delay:3
+    duration: 1,
+    delay: 3,
   })
   .to(".inp", {
     opacity: 1,
@@ -23,50 +23,48 @@ timeline
   .from(".shape", {
     opacity: 0,
     ease: "power4.inOut",
-    stagger: 1
+    stagger: 1,
   })
 
-.from(".close", {
-  opacity: 0,
-  ease: "power4.inOut",
-  // rotation: 720,
-  
-  
-});
-
-
+  .from(".close", {
+    opacity: 0,
+    ease: "power4.inOut",
+    // rotation: 720,
+  });
 
 //target element
 const modal = document.querySelector("#modal__container");
 const openModal = document.querySelector("#open");
 const closeModal = document.querySelector("#close");
 const inp = document.querySelector("#inp");
+const select = document.querySelector("#select");
 
 //functinality
-openModal.addEventListener("click", () => {  
-modal.showModal();
-timeline.play()
-
+const selected = select.addEventListener("change", () => {
+  const selected = select.value;
+  return selected;
+});
+const inputText = inp.addEventListener("change", () => {
+  const inputText = inp.value;
+  return inputText;
+});
+openModal.addEventListener("click", () => {
+  modal.showModal();
+  timeline.play();
 });
 closeModal.addEventListener("click", () => {
   modal.close();
-  timeline.reverse()
-  
-  console.log(inp.value);
-  let action = inp.value;
-  let div = document.createElement("div");
-  let h3 = document.createElement("h3");
-  h3.textContent = action;
-  console.log(h3);
-  inp.value = " ";
+  timeline.reverse();
 
-  div.append(h3);
-  div.classList.add("action__container");
-  h3.classList.add("action__text");
-  //   i have no idea where to append this into the regular page (deji go run this one)
-  // 
-  //   document.
-  
+  // console.log(inp.value);
+  // let action = inp.value;
+  // let div = document.createElement("div");
+  // let h3 = document.createElement("h3");
+  // h3.textContent = action;
+  // console.log(h3);
+  // inp.value = " ";
+
+  // div.append(h3);
+  // div.classList.add("action__container");
+  // h3.classList.add("action__text");
 });
-
-
